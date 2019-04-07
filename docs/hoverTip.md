69c6e02060012be1f5db458bc912ac2aaf60a7e8
YuAngBoy <1730241541@qq.com>
2019-04-07 14:55:54 +0800

    update hover-tip cpmponent

--- docs/hoverTip.md
+++ docs/hoverTip.md
@@ -1,6 +1,15 @@
 ## HoverTip 提示
 常用于展示鼠标 hover 时的提示信息。
 
-<ayu-hover-tip>
-  <template slot="tip-part">向上的内容就是矫情</template>
+<ayu-hover-tip placement="top-center" theme="dark">
+  <template slot="hover-part"><ayu-button>hover</ayu-button></template>
+  <template slot="tip-part">
+  多行信息dadadada
+    <!-- <img src="https://images.pexels.com/photos/317356/pexels-photo-317356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" style="width:100px;height:100px;"> -->
+  </template>
 </ayu-hover-tip>
+<style>
+/* .ayu-hover-tip {
+  margin-left: 50px;
+} */
+</style>
--- packages/Hover-tip/src/index.vue
+++ packages/Hover-tip/src/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="ayu-hover-tip">
+  <div class="ayu-hover-tip" :class="{'disabled': disabled}" @mouseenter="hoverEnter()" @mouseleave="hoverLeave()">
     <div class="ayu-hover-tip_part">
       <slot name="hover-part">
         <ayu-icon icon-class="question" style="font-size: 18px;"/>
       </slot>
     </div>
-    <div class="ayu-hover-tip_tip" :class="placement">
-      <!-- 用户输入部分 -->
-      <slot name="tip-part"></slot>
-      <!-- 箭头 -->
-      <div class="tip_arrow"></div>
-      <!-- 遮挡箭头在div里面部分 -->
-      <div class="tip_block"></div>
-    </div>
+    <transition :name="transition">
+      <div class="ayu-hover-tip_tip" :class="[placement, theme]" v-if="isShow">
+        <!-- 用户要提示的部分 -->
+        <slot name="tip-part"></slot>
+        <!-- 箭头 -->
+        <div class="tip_arrow"></div>
+        <!-- 遮挡箭头在div里面部分 -->
+        <div class="tip_block"></div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -22,9 +24,34 @@ export default {
   props: {
     placement: {
       type: String,
-      default: 'top-center'
+      default: 'top-center' // left/right/top/bottom
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    transition: {
+      type: String,
+      default: 'fade-in-linear' // custom
+    },
+    theme: {
+      type: String,
+      default: 'light' // dark
     }
-  }
+  },
+  data() {
+    return {
+      isShow: false
+    }
+  },
+  methods: {
+    hoverEnter() {
+      this.isShow = true;
+    },
+    hoverLeave() {
+      this.isShow = false;
+    }
+  },
 }
 </script>
 
@@ -37,42 +64,117 @@ export default {
     align-items: center;
   }
   .ayu-hover-tip_tip {
-    padding: 20px;
+    position: absolute;
+    padding: 10px;
     width: max-content;
     white-space: normal;
     word-break: break-all;
     border-radius: 4px;
-    background-color: #fff;
     border-color: transparent #fff transparent transparent;
     box-shadow: 0 0 4px 0 #babec1;
+    font-size: 12px;
     z-index: 2;
     .tip_arrow {
       position: absolute;
-      width: 16px;
-      height: 16px;
-      background-color: #fff;
+      width: 12px;
+      height: 12px;
       box-shadow: 0 0 4px 0 #babec1;
     }
     .tip_block {
       position: absolute;
-      background: #fff;
+    }
+    // :theme light
+    &.light {
+      background-color: #fff;
+      .tip_arrow,
+      .tip_block {
+        background-color: #fff;
+      }
+    }
+    // :theme dark
+    &.dark {
+      color: #fff;
+      box-shadow: none;
+      background-color: rgba($color: #000000, $alpha: 1);
+      .tip_arrow {
+        box-shadow: none;
+        background-color: rgba($color: #000000, $alpha: 1);
+      }
+      .tip_block {
+        background-color: transparent;
+      }
     }
   }
+  // :placement top/bottom/left/right -center
   .top-center {
-    position: absolute;
     bottom: 100%;
     left: 50%;
-    margin-bottom: 8px;
+    margin-bottom: 10px;
     transform: translateX(-50%);
     .tip_arrow {
+      left: 50%;
+      top: 100%;
+      transform: translateY(-50%) translateX(-50%) rotate(45deg);
+    }
+    .tip_block {
+      left: 50%;
+      width: 20px;
+      height: 10px;
+      transform: translateX(-50%);
+    }
+  }
+  .bottom-center {
+    top: 100%;
+    left: 50%;
+    margin-top: 10px;
+    transform: translateX(-50%);
+    .tip_arrow {
+      bottom: 100%;
       left: 50%;
       transform: translateY(50%) translateX(-50%) rotate(45deg);
     }
     .tip_block {
       left: 50%;
-      width: 30px;
+      bottom: 100%;
+      width: 20px;
+      height: 10px;
+      transform: translateX(-50%) translateY(100%);
+    }
+  }
+  .left-center {
+    bottom: 50%;
+    right: 100%;
+    margin-right: 10px;
+    transform: translateY(50%);
+    .tip_arrow {
+      left: 100%;
+      top: 50%;
+      transform: translateY(-50%) translateX(-50%) rotate(45deg);
+    }
+    .tip_block {
+      left: 100%;
+      top: 50%;
+      width: 10px;
       height: 20px;
-      transform: translateX(-50%);
+      transform: translateX(-100%) translateY(-50%);
+    }
+  }
+  .right-center {
+    bottom: 50%;
+    left: 100%;
+    margin-left: 10px;
+    transform: translateY(50%);
+    .tip_arrow {
+      left: 0%;
+      top: 50%;
+      transform: translateY(-50%) translateX(-50%) rotate(45deg);
+    }
+    .tip_block {
+      left: 0%;
+      top: 50%;
+      width: 10px;
+      height: 20px;
+      transform: translateY(-50%);
     }
   }
 }
