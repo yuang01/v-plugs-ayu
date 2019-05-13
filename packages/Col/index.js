946e56ef4569a6ddd2418de742ad0853294674fc
Administrator <1429830548@qq.com>
2019-05-13 09:24:53 +0800

    update

--- packages/Col/index.js
+++ packages/Col/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Col from './src/index.vue'
 
 // 为组件提供 install 安装方法，供按需引入
@@ -8,14 +7,3 @@ Col.install = function (Vue) {
 
 // 默认导出组件
 export default Col
-=======
-import Col from './src/index.vue'
-
-// 为组件提供 install 安装方法，供按需引入
-Col.install = function (Vue) {
-  Vue.component(Col.name, Col)
-}
-
-// 默认导出组件
-export default Col
->>>>>>> dev
