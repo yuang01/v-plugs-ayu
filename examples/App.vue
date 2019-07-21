92c3344084d0c0c388a19c18a8a4a3f9644ace78
YuAngBoy <1730241541@qq.com>
2019-07-21 12:56:52 +0800

    update

--- examples/App.vue
+++ examples/App.vue
@@ -7,11 +7,6 @@
 <script>
 export default {
   mounted() {
-    this.$notify({
-      title: '找工作',
-      content: '本人正在找工作，主要从事vue，有意向的联系我，非常感谢，电话: 18171766796，请大佬相信我的实力',
-      autoClose: false
-    })
   }
 }
 </script>
