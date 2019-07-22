ac6f54a025a5553a30a495621f67040b9acf838c
YuAngBoy <1730241541@qq.com>
2019-07-22 21:08:27 +0800

    增加在线讨论

--- examples/App.vue
+++ examples/App.vue
@@ -1,12 +1,93 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <div class="my-special-button" v-if="isShow" @mouseover="show" @mouseout="hide">
+      <div class="my-special-button__content">
+        在线讨论<ayu-icon icon-class="tickets" class="blink"></ayu-icon>
+      </div>
+      <ayu-icon v-if="showClose" icon-class="close" class="close" @click.stop.native="close"></ayu-icon>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isShow: true,
+      showClose: false,
+    }
+  },
+  methods: {
+    close() {
+      this.isShow = false;
+    },
+    show() {
+      this.showClose = true;
+    },
+    hide() {
+      this.showClose = false;
+    }
+  },
   mounted() {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.my-special-button {
+  position: fixed;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: $color-primary;
+  padding: 10px;
+  color: #fff;
+  user-select: none;
+  cursor: pointer;
+  .my-special-button__content {
+    width: 20px;
+    font-size: 20px;
+    word-wrap: break-word;
+    .blink {
+      animation: blink 1s linear infinite;  
+      /* 其它浏览器兼容性前缀 */
+      -webkit-animation: blink 1s linear infinite;
+      -moz-animation: blink 1s linear infinite;
+      -ms-animation: blink 1s linear infinite;
+      -o-animation: blink 1s linear infinite;
+    }
+  }
+  .close {
+    position: absolute;
+    top: -12px;
+    right: 2px;
+    font-size: 12px;
+    color: $color-primary;
+  }
+}
+
+// 文字闪烁
+/* 定义keyframe动画，命名为blink */
+@keyframes blink{
+  0%{opacity: 1;}
+  100%{opacity: 0;} 
+}
+/* 添加兼容性前缀 */
+@-webkit-keyframes blink {
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+}
+@-moz-keyframes blink {
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+}
+@-ms-keyframes blink {
+  0% {opacity: 1; } 
+  100% { opacity: 0;}
+}
+@-o-keyframes blink {
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+}
+</style>
--- examples/router.js
+++ examples/router.js
@@ -14,96 +14,95 @@ export default new Router({
       component: Home
     },
     {
-      path: '',
-      component: () =>
-      import(/* webpackChunkName: "about" */ "./components/Layout/index.vue"),
+      path: "",
+      component: () => import("./components/Layout/index.vue"),
       children: [
         {
-          path: '/install',
-          name: 'install',
-          component: () => import( "../docs/install.md")
+          path: "/install",
+          name: "install",
+          component: () => import("../docs/install.md")
         },
         {
-          path: '/button',
-          name: 'button',
-          component: () => import( "../docs/button.md")
+          path: "/button",
+          name: "button",
+          component: () => import("../docs/button.md")
         },
         {
-          path: '/floatinput',
-          name: 'floatinput',
-          component: () => import( "../docs/floatinput.md")
+          path: "/floatinput",
+          name: "floatinput",
+          component: () => import("../docs/floatinput.md")
         },
         {
-          path: '/pagination',
-          name: 'pagination',
-          component: () => import( "../docs/pagination.md")
+          path: "/pagination",
+          name: "pagination",
+          component: () => import("../docs/pagination.md")
         },
         {
-          path: '/select',
-          name: 'select',
-          component: () => import( "../docs/select.md")
+          path: "/select",
+          name: "select",
+          component: () => import("../docs/select.md")
         },
         {
-          path: '/carousel',
-          name: 'select',
-          component: () => import( "../docs/carousel.md")
+          path: "/carousel",
+          name: "carousel",
+          component: () => import("../docs/carousel.md")
         },
         {
-          path: '/messageBox',
-          name: 'messageBox',
-          component: () => import( "../docs/messageBox.md")
+          path: "/messageBox",
+          name: "messageBox",
+          component: () => import("../docs/messageBox.md")
         },
         {
-          path: '/icon',
-          name: 'icon',
-          component: () => import( "../docs/icon.md")
+          path: "/icon",
+          name: "icon",
+          component: () => import("../docs/icon.md")
         },
         {
-          path: '/hoverTip',
-          name: 'hoverTip',
-          component: () => import( "../docs/hoverTip.md")
+          path: "/hoverTip",
+          name: "hoverTip",
+          component: () => import("../docs/hoverTip.md")
         },
         {
-          path: '/input',
-          name: 'input',
-          component: () => import( "../docs/input.md")
+          path: "/input",
+          name: "input",
+          component: () => import("../docs/input.md")
         },
         {
-          path: '/message',
-          name: 'message',
-          component: () => import( "../docs/message.md")
+          path: "/message",
+          name: "message",
+          component: () => import("../docs/message.md")
         },
         {
-          path: '/notification',
-          name: 'notification',
-          component: () => import( "../docs/notification.md")
+          path: "/notification",
+          name: "notification",
+          component: () => import("../docs/notification.md")
         },
         {
-          path: '/layout',
-          name: 'layout',
-          component: () => import( "../docs/layout.md")
+          path: "/layout",
+          name: "layout",
+          component: () => import("../docs/layout.md")
         },
         {
-          path: '/radio',
-          name: 'radio',
-          component: () => import( "../docs/radio.md")
+          path: "/radio",
+          name: "radio",
+          component: () => import("../docs/radio.md")
         },
         {
-          path: '/checkbox',
-          name: 'checkbox',
-          component: () => import( "../docs/checkbox.md")
+          path: "/checkbox",
+          name: "checkbox",
+          component: () => import("../docs/checkbox.md")
         },
         {
-          path: '/circle',
-          name: 'circle',
-          component: () => import( "../docs/circle.md")
+          path: "/circle",
+          name: "circle",
+          component: () => import("../docs/circle.md")
         },
         {
-          path: '/inputNumber',
-          name: 'inputNumber',
-          component: () => import( "../docs/inputNumber.md")
-        },
+          path: "/inputNumber",
+          name: "inputNumber",
+          component: () => import("../docs/inputNumber.md")
+        }
       ]
-    },
+    }
   ]
 });
